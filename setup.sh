c785ac3b1e41ef1129bdfac2eba1be9511f008cc
Zinful <zinful@tutamail.com>
2025-09-22 18:58:32 +0000

    yay

---
+++ setup.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+read -p "Is the usb with the right files mounted? (y/n) >>> " confirm
+
+if [[ $confirm == [nN] ]]; then
+	exit 1
+fi
+
+read -p "Enter username used to login >>> " user
+
+echo "getting NIC info."
+
+sleep 1
+
+echo "1. Broadcom"
+echo "2. Intel"
+echo "3. Realtek"
+echo "4. Antheros"
+echo " "
+lspci | grep -i network
+echo " "
+
+read -p "Based off the output, choose the number matching your Driver" num
+
+case $num in
+	1)
+		driver="brcmsmac"
+		;;	
+	2)
+		driver="iwlwifi"
+		;;
+	3)
+		driver="r8169"
+		;;
+	4)
+		driver="anth9k"
+		;;
+	*)
+		echo "invalid"
+		exit 1
+		;;
+esac
+
+echo "Selected $driver"
+
+echo "continuing install in 5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+
+echo "making catastrophic changes"
+
+sleep 1
+
+apt update
+
+apt install xserver-xorg-input-libinput xserver-xorg-input-evdev \
+xserver-xorg-video-ati xserver-xorg-video-fbdev \
+xserver-xorg-video-vesa xterm cmst cbatticon acpi light-locker \
+sakura fluxbox-themes-antix tint2 connman pcmanfm clipit \
+python3 ssh wireguard putty gcc make wireless-regdb -y 
+
+read -p "install steam? (y/n) >>> " confirm
+
+if [[ $confirm == [yY] ]]; then
+	apt install steam-launcher
+	sudo /usr/bin/steamdeps --interactive --update-apt
+fi
+
+echo "Setting up user menu"
+sleep 1
+cat /mnt/usr_menu > /home/$user/.fluxbox/menu
+echo "setting up sys menu"
+sleep 1
+cat /mnt/sys_menu > /etc/X11/fluxbox/fluxbox-menu
+echo "setting up startup procs"
+sleep 1
+cat /mnt/startup > /home/$user/.fluxbox/startup
+echo "setting up wireless module load"
+sleep 1
+echo $driver | tee -a /etc/modules
+echo "rebooting in 5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+reboot
