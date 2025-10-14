098a11e37df817975066f4b0773009eee13d8691
Zinful <zinful@tutamail.com>
2025-10-14 17:31:33 -0400

    Add files via upload

---
+++ workstation_setup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+read -p "Is the usb with the right files mounted? (y/n) >>> " confirm
+
+if [[ $confirm == [nN] ]]; then
+	exit 1
+fi
+
+read -p "Enter username used to login >>> " user
+
+echo "getting NIC info."
+
+sleep 1
+
+lspci | grep -i network
+
+echo "continuing install in 5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+
+echo "making catastrophic changes"
+
+sleep 1
+
+apt update
+
+echo "installing apps"
+
+sleep 1
+
+
+apt install nano curl unzip zip ampy htop iotop strace ltrace screen dfu-util \
+avrdude python3-full python3-venv python3-pip python3-dev gdb libasound2-dev \
+portaudio19-dev alsa-utils ssh wireguard uefitool-cli gcc binwalk flashrom make \
+wireless-regdb build-essential git cmake libpci-dev lmsensors micropython-mpremote\
+nmap hexdump ripgrep esptool picocom minicom hexdiff lzop chipsec acpica-tools me_cleaner\
+ufei-firmware-parser fdisk cfdisk parted testdisk radare2 objdump ghidra ndisasm\
+dmidecode pciutils usbutils spi-tools i2c-tools coreboot-utils nvramtool efivar efibootmgr\
+pipx tcpdump ethtool open-jdk-25 -y 
+
+echo "downloading micropy for rp2040"
+
+wget https://micropython.org/resources/firmware/rp2-pico-20241130.uf2
+
+sleep 1
+
+
+
+sleep 1
+
+read -p "Install zin-apps?" confirm
+
+if [[ $confirm == [yY] ]]; then
+	git clone https://github.com/Zin-ful/zin-minimal-suite	
+fi
+
+echo "setting up wireguard"
+
+cp -r /mnt/wgup /etc/runit/runsvdir/default
+
+echo "runit service made"
+
+echo "rebooting in 5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+reboot
